38c1386e28b821f0d22cd947e33b72e342a9e5cd
vpdiongzon <vpdiongzon@gmail.com>
2024-01-19 10:47:08 +0800

    adding data reactivity for custom element demo

--- src/components/chat-window.ce.vue
+++ src/components/chat-window.ce.vue
@@ -1,5 +1,29 @@
 <template>
     <div>
         <label for="">New Message</label>
+        <input type="text" v-model="data.text">˝
+        <button @click="triggerSubmit">submit</button>
+        <div>
+            <h1>messages</h1>
+            <p v-for="(item, idx) in data.chat" :key="idx">
+                {{ item }}
+            </p>
+        </div>
     </div>
 </template>
+
+<script setup>
+import { reactive, ref } from 'vue';
+const chat = "";
+
+const data = reactive({
+    chat: [],
+    "text": ""
+});
+
+const triggerSubmit = ()=> {
+    data.chat.push(data.text);
+    data.text = ""
+}
+
+</script>
